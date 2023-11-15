d56cae12683ec90826009c238d955b3264e7102d
HyperShivam1 <shivammittalgg@gmail.com>
2023-11-15 13:02:59 +0530

    some changes

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -79,7 +79,7 @@
               <a class="nav-link" href="blog.html">News</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contac</a>
+              <a class="nav-link" href="contact.html">Contact</a>
             </li>
 
             @if(Route::has('login'))
